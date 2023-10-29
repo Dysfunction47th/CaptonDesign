24c582efd0439c64b4b6d57091aa54f4c58cc329
gguggu <geonuk1115@gmail.com>
2023-10-29 12:35:33 +0900

    .

--- client.py
+++ client.py
@@ -4,12 +4,15 @@ import sys
 
 clientSock = socket(AF_INET, SOCK_STREAM)
 # clientSock.connect(('127.168.25.31', 8080))
-clientSock.connect(('127.168.50.1', 8080))
+#clientSock.connect(('127.168.50.1', 8080))
+clientSock.connect(('192.168.50.153', 8080))
 
 
 
 print('연결에 성공했습니다.')
-filename = "data/data"
+# filename = "data/data"
+filename = "rrrr.txt"
+
 clientSock.sendall(filename.encode('utf-8'))
 
 data = clientSock.recv(1024)
